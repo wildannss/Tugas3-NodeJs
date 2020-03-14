b03fccbd4d48bf315fe2276c91d185e8297e529e
wildan <wildan.syarif1021@gmail.com>
2020-03-14 21:26:55 +0700

    tugas niomic
    tugas niomic

---
+++ api-routes.js
@@ -0,0 +1,19 @@
+let router = require('express').Router()
+
+router.get('/', function(req,res){
+    res.json({
+        status: "API working",
+        pesan: "Selamat Datang"
+    });
+});
+
+let siswaController = require('./siswaController');
+
+router.route('/siswa')
+    .get(siswaController.index)
+    .post(siswaController.new);
+router.route('/siswa/:siswa_id')
+    .get(siswaController.view)
+    .put(siswaController.put);
+
+module.exports = router;
---
+++ siswaController.js
@@ -0,0 +1,80 @@
+siswaModel = require('./siswaModel');
+
+// home/tampilkan
+exports.index = function(req,res){
+    siswaModel.get(function (err, siswa) {
+        if(err){
+            res.json({
+                status: "error",
+                pesan: err
+            });
+        }
+        res.json({
+            status: "sukses",
+            pesan: "Menampilkan data siswa",
+            data: siswa
+        });
+    });
+};
+
+// input baru
+exports.new = function(req,res){
+    let siswa = new siswaModel();
+    siswa.nama = req.query.nama ? req.query.nama : siswa.nama;
+    siswa.tanggallahir = req.query.tanggallahir;
+    siswa.jeniskelamin = req.query.jeniskelamin;
+    siswa.hobi = req.query.hobi;
+
+    //save
+    siswa.save(function(err){
+        if(err)
+        res.json(err);
+        res.json({
+            pesan: "Data siswa sudah ditambah",
+            data: siswa
+        });
+    });
+};
+
+// cari info
+exports.view = function(req,res){
+    siswaModel.findById(req.params.siswa_id, function (err, siswa) {
+        if(err)
+        res.send(err);
+        res.json({
+            pesan: "Data siswa sudah di loading ...",
+            data: siswa
+        });
+    });
+};
+
+//update data
+exports.put = function(req,res){
+    siswaModel.findById(req.params.siswa_id, function (err,siswa){
+        if(err)
+        res.send(err);
+
+        siswa.nama = req.query.nama ? req.query.nama : siswa.nama;
+        siswa.tanggallahir = req.query.tanggallahir;
+        siswa.jeniskelamin = req.query.jeniskelamin;
+        siswa.hobi = req.query.hobi;
+
+        //save
+        siswa.save(function (err) {
+            if (err)
+                res.json(err);
+            res.json({
+                pesan: "Siswa sudah diupdate",
+                data: siswa
+            });
+        }); 
+    });
+    siswaModel.findById(req.param.siswa_id, function (err, siswa) {
+        if(err)
+        res.send(err);
+        res.json({
+            pesan: "Data siswa sedang di loading ...",
+            data: siswa
+        });
+    });
+};
